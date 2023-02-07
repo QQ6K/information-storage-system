65de2833b019d7a2c5b807bd0384a00d97d0a597
QQ6K <golyshev491@gmail.co>
2023-02-07 22:22:02 +0300

    init

---
+++ src/main/java/ru/task/iss/InformationStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.task.iss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationStorageSystemApplication.class, args);
+	}
+
+}
